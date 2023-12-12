4900ed2a8f71abe02c6b7ad2c51bffcc6ed955af
Abdulrehmaniqbal786 <147655636+Abdulrehmaniqbal786@users.noreply.github.com>
2023-12-12 01:30:03 -0800

    Complete

---
+++ app.js
@@ -0,0 +1,98 @@
+[
+  {
+  "id": 4,
+  "title": "newTitle",
+  "price": 205,
+  "description": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+  "image_url": "https://i.imgur.com/R2PN9Wq.jpeg"
+  },
+  {
+  "id": 7,
+  "title": "Ergonomic Wooden Tuna",
+  "price": 743,
+  "description": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+  "image_url": "https://i.imgur.com/mp3rUty.jpeg"
+  },
+  {
+  "id": 9,
+  "title": "Electronic Bronze Chips",
+  "price": 808,
+  "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+  "image_url": "https://i.imgur.com/R3iobJA.jpeg"
+  },
+  {
+  "id": 11,
+  "title": "Awesome Bronze Car",
+  "price": 382,
+  "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+  "image_url": "https://i.imgur.com/cBuLvBi.jpeg"
+  },
+  {
+  "id": 12,
+  "title": "Recycled Rubber Cheese",
+  "price": 30,
+  "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+  "image_url": "https://i.imgur.com/KeqG6r4.jpeg"
+
+  
+  }
+  ];
+ const card1 = document.getElementById("card1");
+ card1.innerHTML=`<div class="card" style="width: 18rem;">
+ <img src="https://i.imgur.com/R2PN9Wq.jpeg" class="card-img-top" alt="...">
+ <div class="card-body">
+   <h5 class="card-title">newTitle</h5>
+   <p class="card-text">The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality.</p>
+   <a href="#" class="btn btn-primary">Order Now</a>
+ </div>
+</div>`
+
+const card2 = document.getElementById("card2");
+card2.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://i.imgur.com/mp3rUty.jpeg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Ergonomic Wooden Tuna</h5>
+  <p class="card-text">Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles.</p>
+  <a href="#" class="btn btn-primary">Order Now</a>
+</div>
+</div>`
+
+const card3 = document.getElementById("card3");
+card3.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://i.imgur.com/R3iobJA.jpeg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Electronic Bronze Chips</h5>
+  <p class="card-text">The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive.</p>
+  <a href="#" class="btn btn-primary">Order Now</a>
+</div>
+</div>`
+const card4 = document.getElementById("card4");
+card4.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://i.imgur.com/cBuLvBi.jpeg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Awesome Bronze Car</h5>
+  <p class="card-text">Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals.</p>
+  <a href="#" class="btn btn-primary">Order Now</a>
+</div>
+</div>`
+
+const card5 = document.getElementById("card5")
+card5.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://i.imgur.com/KeqG6r4.jpeg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Recycled Rubber Cheese</h5>
+  <p class="card-text">New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016.</p>
+  <a href="#" class="btn btn-primary">Order Now</a>
+</div>
+</div>`
+
+const card6 = document.getElementById("card6");
+card6.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://img.fruugo.com/product/5/03/1090186035_max.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Awesome smart watch</h5>
+  <p class="card-text">Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals.</p>
+  <a href="#" class="btn btn-primary">Order Now</a>
+</div>
+</div>`
+
